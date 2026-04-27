679ac26290f2ec06eef559f1b12cf3116898c8bc
Aiden Domingo <adomingo@sandiego.edu>
2026-04-27 00:04:27 -0700

    fix: used checkstyle to add newlines to end of file and place a private constructor in utility class so instance is not accidentally created.

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -1,6 +1,11 @@
 package edu.sandiego.comp305;
 
 public class StringUtils {
+
+    private StringUtils() {
+        // Empty constructor to satisfy checkstyle
+    }
+
     public static String reverseString(String str) {
         return new StringBuilder(str).reverse().toString();
     }
